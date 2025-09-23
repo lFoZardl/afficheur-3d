f6e4508e6272960bda0cfc70f506f5361fe15115
FoZard <fozard.fz@gmail.com>
2025-09-23 18:09:27 -0400

    devices et queue families

--- src/main.zig
+++ src/main.zig
@@ -14,6 +14,15 @@ const math = @import("maths.zig");
 
 const debugVulkan = true;
 
+const QueueFamilyIndices = struct {
+    const Self = @This();
+    graphics_family: ?u32 = null,
+
+    fn isComplete(self: Self) bool {
+        return self.graphics_family != null;
+    }
+};
+
 const Application = struct {
     const Self = @This();
 
@@ -22,34 +31,29 @@ const Application = struct {
     vki: vk.InstanceWrapper = undefined,
     instance: vk.Instance = vk.Instance.null_handle,
     debug_messenger: vk.DebugUtilsMessengerEXT = .null_handle,
+    physical_device: vk.PhysicalDevice = .null_handle,
 
     const Erreur = vk.BaseWrapper.CreateInstanceError || error{
         GlfwInit,
         CreationFenetre,
         GlfwGetRequiredInstanceExtensions,
-        EnumerateInstanceExtensionProperties,
-        EnumerateInstanceLayerProperties,
     };
 
     pub fn getInstanceExtensions(self: Self) !std.ArrayList(vk.ExtensionProperties) {
         return enumerateExtensions(self.vkb);
     }
-    pub fn enumerateExtensions(vkb: vk.BaseWrapper) !std.ArrayList(vk.ExtensionProperties) {
-        var nb_extensions: u32 = undefined;
-        const resultat1 = vkb.enumerateInstanceExtensionProperties(null, &nb_extensions, null) catch |err| {
-            return err;
-        };
-        if (resultat1 != .success) return Self.Erreur.EnumerateInstanceExtensionProperties;
+    fn enumerateExtensions(vkb: vk.BaseWrapper) !std.ArrayList(vk.ExtensionProperties) {
+        var count: u32 = undefined;
+        const resultat1 = try vkb.enumerateInstanceExtensionProperties(null, &count, null);
+        if (resultat1 != .success) return error.Unknown;
 
-        var list = try std.ArrayList(vk.ExtensionProperties).initCapacity(gpa, nb_extensions);
+        var list = try std.ArrayList(vk.ExtensionProperties).initCapacity(gpa, count);
         errdefer list.deinit(gpa);
 
-        const resultat2 = vkb.enumerateInstanceExtensionProperties(null, &nb_extensions, @ptrCast(list.items)) catch |err| {
-            return err;
-        };
-        if (resultat2 != .success) return Self.Erreur.EnumerateInstanceExtensionProperties;
+        const resultat2 = try vkb.enumerateInstanceExtensionProperties(null, &count, @ptrCast(list.items));
+        if (resultat2 != .success) return error.Unknown;
 
-        list.items.len = nb_extensions;
+        list.items.len = count;
         return list;
     }
 
@@ -57,58 +61,107 @@ const Application = struct {
         return enumerateLayers(self.vkb);
     }
     fn enumerateLayers(vkb: vk.BaseWrapper) !std.ArrayList(vk.LayerProperties) {
-        var nb_layers: u32 = undefined;
-        const resultat1 = vkb.enumerateInstanceLayerProperties(&nb_layers, null) catch |err| {
-            return err;
-        };
-        if (resultat1 != .success) return Self.Erreur.EnumerateInstanceLayerProperties;
+        var count: u32 = undefined;
+        const resultat1 = try vkb.enumerateInstanceLayerProperties(&count, null);
+        if (resultat1 != .success) return error.Unknown;
 
-        var list = try std.ArrayList(vk.LayerProperties).initCapacity(gpa, nb_layers);
+        var list = try std.ArrayList(vk.LayerProperties).initCapacity(gpa, count);
         errdefer list.deinit(gpa);
 
-        const resultat2 = vkb.enumerateInstanceLayerProperties(&nb_layers, @ptrCast(list.items)) catch |err| {
-            return err;
-        };
-        if (resultat2 != .success) return Self.Erreur.EnumerateInstanceLayerProperties;
+        const resultat2 = try vkb.enumerateInstanceLayerProperties(&count, @ptrCast(list.items));
+        if (resultat2 != .success) return error.Unknown;
 
-        list.items.len = nb_layers;
+        list.items.len = count;
         return list;
     }
 
-    fn debugCallback(msg_severite: vk.DebugUtilsMessageSeverityFlagsEXT, msg_type: vk.DebugUtilsMessageTypeFlagsEXT, p_callback_data: ?*const vk.DebugUtilsMessengerCallbackDataEXT, _: ?*anyopaque) callconv(vk.vulkan_call_conv) vk.Bool32 {
+    fn enumeratePhysicalDevices(self: Self) !std.ArrayList(vk.PhysicalDevice) {
+        var count: u32 = undefined;
+        const resultat1 = try self.vki.enumeratePhysicalDevices(self.instance, &count, null);
+        if (resultat1 != .success) return error.Unknown;
+
+        var list = try std.ArrayList(vk.PhysicalDevice).initCapacity(gpa, count);
+        errdefer list.deinit(gpa);
+
+        const resultat2 = try self.vki.enumeratePhysicalDevices(self.instance, &count, @ptrCast(list.items));
+        if (resultat2 != .success) return error.Unknown;
+
+        list.items.len = count;
+        return list;
+    }
+
+    fn debugCallback(
+        message_severity: vk.DebugUtilsMessageSeverityFlagsEXT,
+        message_types: vk.DebugUtilsMessageTypeFlagsEXT,
+        p_callback_data: ?*const vk.DebugUtilsMessengerCallbackDataEXT,
+        _: ?*anyopaque,
+    ) callconv(vk.vulkan_call_conv) vk.Bool32 {
         assert(p_callback_data != null);
-        const str_couleur_debut =
-            if (msg_severite.error_bit_ext)
+        const str_severity =
+            if (message_severity.error_bit_ext)
                 terminal.Color.red ++ "ERROR"
-            else if (msg_severite.warning_bit_ext)
+            else if (message_severity.warning_bit_ext)
                 terminal.Color.yellow ++ "WARNING" //"\x1B[33mWARNING"
-            else if (msg_severite.info_bit_ext)
+            else if (message_severity.info_bit_ext)
                 terminal.Color.cyan ++ "INFO"
-            else if (msg_severite.verbose_bit_ext)
+            else if (message_severity.verbose_bit_ext)
                 "VERBOSE"
             else
-                "[UNKNOWN SEVERITY]\n\t";
+                "[UNKNOWN SEVERITY]";
         const str_type =
-            if (msg_type.general_bit_ext)
+            if (message_types.general_bit_ext)
                 "general"
-            else if (msg_type.validation_bit_ext)
+            else if (message_types.validation_bit_ext)
                 "validation"
-            else if (msg_type.performance_bit_ext)
+            else if (message_types.performance_bit_ext)
                 "performance"
-            else if (msg_type.device_address_binding_bit_ext)
+            else if (message_types.device_address_binding_bit_ext)
                 "device_address_binding"
             else
                 "[UNKOWN TYPE]";
 
+        const str_message = (if (p_callback_data) |p|
+            p.p_message orelse null
+        else
+            null) orelse
+            "[???]";
+
+        //{
+        //    var str_objets = try std.ArrayList(u8).initCapacity(gpa, 4);
+        //    defer str_objets.deinit(gpa);
+        //    try str_objets.appendSlice(gpa, "[ ");
+        //    if (p_callback_data) |p| {
+        //        if (p.object_count > 0) {
+        //            const objets = p.p_objects.?[0..p.object_count];
+        //            for (objets, 0..) |objet, i| {
+        //                if (i > 0) {
+        //                    try str_objets.appendSlice(gpa, ", ");
+        //                }
+        //                try str_objets.appendSlice(gpa, objet.p_object_name orelse "???");
+        //            }
+        //        }
+        //    }
+        //    try str_objets.appendSlice(gpa, " ]");
+        //}
+        //catch {
+        //// jsp quoi faire dans ce cas mdr
+        //std.debug.panic("allocation failure", .{});
+        //};
+
         std.log.debug(
+            // severity
             terminal.Set.bold ++ terminal.Set.underline ++ "{s}" ++
-                " {s}\n\t" ++ terminal.Reset.bold ++ terminal.Reset.underline ++
-                terminal.Set.italic ++ "{s}" ++ terminal.Reset.italic ++
+                // type
+                " {s}" ++ terminal.Reset.bold ++ terminal.Reset.underline ++
+                // message
+                "\n\t" ++ terminal.Set.italic ++ "{s}" ++ terminal.Reset.italic ++
+                // objets
+                "\n" ++ //terminal.Set.dim ++ "{s}" ++ terminal.Reset.dim ++
                 terminal.Color.default,
             .{
-                str_couleur_debut,
+                str_severity,
                 str_type,
-                p_callback_data.?.p_message.?,
+                str_message,
             },
         );
 
@@ -146,19 +199,63 @@ const Application = struct {
         return list;
     }
 
+    fn findQueueFamilies(self: Self, device: vk.PhysicalDevice) !QueueFamilyIndices {
+        var indices: QueueFamilyIndices = .{};
+
+        var queue_family_count: u32 = 0;
+        self.vki.getPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, null);
+
+        var queue_families = try std.ArrayList(vk.QueueFamilyProperties).initCapacity(gpa, queue_family_count);
+        defer queue_families.deinit(gpa);
+        self.vki.getPhysicalDeviceQueueFamilyProperties(device, &queue_family_count, queue_families.items.ptr);
+
+        for (queue_families.items, 0..) |queue_family, i| {
+            if (queue_family.queue_flags.graphics_bit) {
+                indices.graphics_family = @intCast(i);
+            }
+
+            if (indices.isComplete()) {
+                break;
+            }
+        }
+
+        return indices;
+    }
+    fn isDeviceSuitable(self: Self, device: vk.PhysicalDevice) !bool {
+        const indices = try self.findQueueFamilies(device);
+
+        return indices.isComplete();
+    }
+    fn pickPhysicalDevice(self: *Self) !void {
+        var devices = try self.enumeratePhysicalDevices();
+        defer devices.deinit(gpa);
+
+        for (devices.items) |device| {
+            if (try self.isDeviceSuitable(device)) {
+                self.physical_device = device;
+                break;
+            }
+        }
+        if (self.physical_device == .null_handle) {
+            std.log.err("no supported physical device found", .{});
+            return error.Unknown;
+        }
+    }
+
     pub fn init() !Self {
+        var self: Self = undefined;
         if (glfw.init() == 0) {
             return Erreur.GlfwInit;
         }
 
-        const fenetre = glfw.Window.create(200, 200, "allo", null, null) catch {
+        self.fenetre = glfw.Window.create(200, 200, "allo", null, null) catch {
             std.debug.print("fen invalide\n", .{});
             return Erreur.CreationFenetre;
         };
 
-        //déb vk
+        //déb vk instance
         const vk_proc: *const fn (instance: vk.Instance, procname: [*:0]const u8) callconv(.c) vk.PfnVoidFunction = @ptrCast(&glfw.getInstanceProcAddress);
-        const vkb = vk.BaseWrapper.load(vk_proc);
+        self.vkb = vk.BaseWrapper.load(vk_proc);
 
         const app_info = vk.ApplicationInfo{
             .p_application_name = "Afficheur 3D",
@@ -190,24 +287,39 @@ const Application = struct {
             create_info.p_next = &debug_create_info;
         }
 
-        const instance = try vkb.createInstance(&create_info, null);
-        const vki = vk.InstanceWrapper.load(instance, vk_proc);
-        //fin vk
+        self.instance = try self.vkb.createInstance(&create_info, null);
+        self.vki = vk.InstanceWrapper.load(self.instance, vk_proc);
+        //fin vk instance
 
-        return .{
-            .fenetre = fenetre,
-            .vkb = vkb,
-            .vki = vki,
-            .instance = instance,
-        };
+        //déb vk debug messenger
+        self.debug_messenger = .null_handle;
+        if (debugVulkan) {
+            var messenger_create_info: vk.DebugUtilsMessengerCreateInfoEXT = undefined;
+            populateDebugMessengerCreateInfo(&messenger_create_info);
+
+            self.debug_messenger = try self.vki.createDebugUtilsMessengerEXT(self.instance, &messenger_create_info, null);
+        }
+        //fin vk debug messenger
+
+        //déb vk devices
+        try self.pickPhysicalDevice();
+        //fin vk devices
+
+        return self;
     }
 
     pub fn deinit(self: *Self) void {
+        //déb vk debug messenger
         if (debugVulkan and self.debug_messenger != .null_handle) {
             self.vki.destroyDebugUtilsMessengerEXT(self.instance, self.debug_messenger, null);
         }
+        //fin vk debug messenger
+
+        //déb vk instance
         assert(self.instance != .null_handle);
         self.vki.destroyInstance(self.instance, null);
+        //fin vk instance
+
         self.fenetre.destroy();
         glfw.terminate();
     }
